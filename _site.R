d15e3c105b2d4a76ab6e25ca39aeb9727243f014
Ghislaine Jumonville <gj2356@cumc.columbia.edu>
2023-11-02 12:58:13 -0400

    added the site.yml

---
+++ _site.R
@@ -0,0 +1,14 @@
+name: "Ghislaine Jumonville"
+output_dir: "."
+navbar:
+  title: "Home"
+  right:
+    - text: "About"
+      href: about.html
+    - icon: fa-envelope fa-lg
+      href: mailto:<gj2356@cumc.columbia.edu>
+    - icon: fa-github fa-lg
+      href: http://github.com/<ghislainej>/
+output:
+  html_document:
+    theme: cerulean
